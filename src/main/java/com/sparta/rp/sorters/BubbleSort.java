833cfeab9a47ebda30d39e0a45dc4690113f35b5
Roshni <roshnihpatel@outlook.com>
2022-08-10 15:49:11 +0100

    implemented sortArray for BubbleSort Class

--- src/main/java/com/sparta/rp/sorters/BubbleSort.java
+++ src/main/java/com/sparta/rp/sorters/BubbleSort.java
@@ -1,9 +1,36 @@
 package com.sparta.rp.sorters;
 
+import java.util.Arrays;
+
 public class BubbleSort implements Sorter {
 
     @Override
     public int[] sortArray(int[] arrToSort) {
-        return new int[0];
+
+            int[] sortedArray = new int[arrToSort.length];
+            System.arraycopy(arrToSort, 0, sortedArray,0, arrToSort.length);
+            for(int loops= 0; loops < arrToSort.length-1; loops++){
+                sortedArray= oneIterationSort(sortedArray);
+            }
+            System.out.println("Values are "+ Arrays.toString(sortedArray));
+            return sortedArray;
+
+    }
+    private static int[] oneIterationSort(int[] unsortedArray){
+
+        for (int i = 0; i < unsortedArray.length - 1; i++) {
+            if (unsortedArray[i] > unsortedArray[i + 1]) {
+                swap(unsortedArray, i);
+            }
+            //System.out.println("Values are "+ Arrays.toString(unsortedArray));
+        }
+        return unsortedArray;
+    }
+
+    private static void swap(int[] unsortedArray, int i) {
+        int temp;
+        temp = unsortedArray[i];
+        unsortedArray[i] = unsortedArray[i + 1];
+        unsortedArray[i + 1] = temp;
     }
 }
