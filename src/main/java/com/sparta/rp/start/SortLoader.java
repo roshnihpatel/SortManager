d4dd7eeb3699a4cd8225d2eda8ce7594ac7a5172
Roshni <roshnihpatel@outlook.com>
2022-08-11 17:46:04 +0100

    created SortLoader class

---
+++ src/main/java/com/sparta/rp/start/SortLoader.java
@@ -0,0 +1,9 @@
+package com.sparta.rp.start;
+
+import com.sparta.rp.display.Displayer;
+
+public class SortLoader {
+    public static void startSortManager() {
+        Displayer.userInterface();
+    }
+}
