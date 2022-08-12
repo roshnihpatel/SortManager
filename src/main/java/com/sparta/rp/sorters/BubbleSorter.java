7d4df84a06b14ca1ba19f6ace3b1919e39d83946
Roshni <roshnihpatel@outlook.com>
2022-08-12 15:18:39 +0100

    logging sorters

--- src/main/java/com/sparta/rp/sorters/BubbleSorter.java
+++ src/main/java/com/sparta/rp/sorters/BubbleSorter.java
@@ -1,34 +1,56 @@
 package com.sparta.rp.sorters;
 
 import java.util.Arrays;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BubbleSorter implements Sorter {
+    private static final Logger logger = Logger.getLogger("Bubble Sort logger");
+    private static final ConsoleHandler handler = new ConsoleHandler();
+
+
+
+    public static void main(String[] args) {
+
+        handler.setLevel(Level.ALL);
+        logger.addHandler(handler);
+
+        logger.setUseParentHandlers(false);
+        logger.setLevel(Level.FINE);
+
+    }
 
 
     @Override
-    public int[] sortArray(int[] arrToSort) {
+    public int[] sortArray(int[] unsortedArray) {
+       int[] arrToSort = unsortedArray.clone();
 
-
+        logger.log(Level.INFO,"This is the array to sort: " + Arrays.toString(arrToSort));
 
             for(int loops= 0; loops < arrToSort.length-1; loops++){
                 arrToSort= oneIterationSort(arrToSort);
             }
-
+        logger.log(Level.INFO,"This is the sorted array: " + Arrays.toString(arrToSort));
             return arrToSort;
 
     }
     private static int[] oneIterationSort(int[] unsortedArray){
-
+        logger.log(Level.FINE,"This is before an iteration: " + Arrays.toString(unsortedArray) );
         for (int i = 0; i < unsortedArray.length - 1; i++) {
+            logger.log(Level.FINER, "These are the comparisons made:  "+  unsortedArray[i] +">" + unsortedArray[i + 1] +": "+ (unsortedArray[i] > unsortedArray[i + 1])) ;
             if (unsortedArray[i] > unsortedArray[i + 1]) {
                 swap(unsortedArray, i);
             }
-            //System.out.println("Values are "+ Arrays.toString(unsortedArray));
+
         }
+        logger.log(Level.FINE,"This is after an iteration: " + Arrays.toString(unsortedArray) );
         return unsortedArray;
     }
 
     private static void swap(int[] unsortedArray, int i) {
+        logger.log(Level.FINE,"A swap occurred between " + unsortedArray[i] + " and " + unsortedArray[i+1] );
+
         int temp;
         temp = unsortedArray[i];
         unsortedArray[i] = unsortedArray[i + 1];
